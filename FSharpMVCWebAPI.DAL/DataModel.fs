284e5dc48113d65fddf3d51f3c51719079d81bbd
Denys Radchenko <Denys_Radchenko@epam.com>
2016-11-04 12:08:09 +0200

    Added data access layer

---
+++ FSharpMVCWebAPI.DAL/DataModel.fs
@@ -0,0 +1,17 @@
+﻿namespace FSharpMVCWebAPI.DAL
+    open System.ComponentModel.DataAnnotations
+    open System.Data.Entity
+
+
+    type Product() = 
+        let mutable m_ID    : int = 0
+        let mutable m_Name  : string = ""
+        let mutable m_Price : float = 0.0
+
+        [<Key>]
+        member public X.ID    with get() = m_ID
+                              and  set v = m_ID <- v
+        member public X.Name  with get() = m_Name
+                              and  set v = m_Name <- v
+        member public X.Price with get() = m_Price
+                              and  set v = m_Price <- v
---
+++ FSharpMVCWebAPI.DAL/EFDatabase.fs
@@ -0,0 +1,13 @@
+﻿namespace FSharpMVCWebAPI.DAL
+    open System.ComponentModel
+    open System.ComponentModel.DataAnnotations
+    open System.Data.Entity
+
+    type EFDatabase() =
+        inherit DbContext()
+
+        [<DefaultValue>]
+        let mutable m_Products : DbSet<Product>
+
+        member public X.Products   with get() = X.m_Products
+                                   and  set v = X.m_Products <- v
